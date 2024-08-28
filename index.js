ff3a17f39c292f03e0fab22feebd0226b026e5e4
jung <jcy0011@yahoo.com>
2024-08-29 07:27:50 +0900

    [240829] 정치영 과제03
    과제03

--- index.js
+++ index.js
@@ -48,6 +48,24 @@ function JS_수강생상세정보보여주기(수강생번호) {
   );
 }
 
+function change_pw_2_color() {
+  const pwinput1 = document.getElementById("비밀번호입력창").value;
+  const pwinput2 = document.getElementById("비밀번호확인입력창").value;
+  if (pwinput1 !== pwinput2) {
+    document.getElementById("비밀번호확인입력창").style.border = "1px solid red";
+  }
+  else {
+    document.getElementById("비밀번호확인입력창").style.border = "1px solid var(--gray-300, #c7c7c7)";
+  }
+}
+
+function activate_signup() {
+  const btn = document.getElementById("user-register-button")
+  const img = document.querySelector("#register-img")
+  img.src = "./assets/register-button-activated.png";
+  btn.disabled = false;
+}
+
 function JS_회원가입() {
   // 오늘날짜 계산하기
   const 나의날짜 = new Date();
@@ -58,13 +76,12 @@ function JS_회원가입() {
     "-" +
     나의날짜.getDate();
 
-  // 회원가입 완료 alert 창 날짜와 함께 보여주기
-  alert(`회원가입을 축하합니다. \n(가입일시: ${오늘연월일})`);
-
   // 새로운 수강생 추가하기
   const 내가입력한이름 = document.getElementById("이름입력창").value;
   const 내가입력한이메일 = document.getElementById("이메일입력창").value;
   const 내가입력한비밀번호 = document.getElementById("비밀번호입력창").value;
+  const pwinput2 = document.getElementById("비밀번호확인입력창").value;
+  
   const 내가입력한전화번호 = document.getElementById("전화번호입력창").value;
   const 내가입력한자기소개 = document.getElementById("자기소개입력창").value;
 
@@ -93,6 +110,45 @@ function JS_회원가입() {
     가입일시: 오늘연월일,
   };
 
+  const telcheck = /^010-\d{3,4}-\d{3,4}$/;
+  if (!telcheck.test(내가입력한전화번호))
+  {
+    alert (`전화번호 형식이 올바르지 않습니다`);
+    return;
+  } 
+
+  if (!mailcheck(내가입력한이메일))
+  {
+    alert(`이메일 형식이 올바르지 않습니다.`);
+    return;
+  }
+
+function mailcheck(email)
+{
+	if (!email)
+		return false;
+		
+	if(email.length>254)
+		return false;
+
+	var valid = tester.test(email);
+	if(!valid)
+		return false;
+
+	// Further checking of some things regex can't handle
+	var parts = email.split("@");
+	if(parts[0].length>64)
+		return false;
+
+	var domainParts = parts[1].split(".");
+	if(domainParts.some(function(part) { return part.length>63; }))
+		return false;
+
+	return true;
+}
+
+  // 회원가입 완료 alert 창 날짜와 함께 보여주기
+  alert(`회원가입을 축하합니다. \n(가입일시: ${오늘연월일})`);
   수강생모음.push(새로운수강생);
   // 변경된 수강생목록 보여주기
   JS_수강생DOM만들기();
@@ -109,7 +165,7 @@ function 인증번호요청기능() {
   const 인증번호 = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
   document.getElementById("인증번호보여주는곳").innerText = 인증번호;
 
-  let 남은시간 = 180;
+  let 남은시간 = 180-1;
   timer = setInterval(function () {
     const 분 = String(Math.floor(남은시간 / 60)).padStart(2, "0");
     const 초 = String(남은시간 % 60).padStart(2, "0");
@@ -126,6 +182,6 @@ function 인증번호요청기능() {
 }
 
 function JS_인증완료기능() {
-  const 결과버튼 = document.getElementById("회원가입_인증번호_결과버튼");
-  결과버튼.src = "./assets/인증완료.png";
+  const 결과버튼 = document.getElementById("user-register-button");
+  결과버튼.src = "./assets/verify-done.png";
 }
