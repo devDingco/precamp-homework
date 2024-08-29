098d1f9358cd540b15052218474e00d0714f02a3
jintae1954 <qkrwlsxo1954@gmail.com>
2024-08-29 16:34:15 +0900

    [240829] 프리캠프 section03 과제

--- homework/index.js
+++ homework/index.js
@@ -7,24 +7,127 @@ const year = String(d.getFullYear());
 const month = String(d.getMonth() + 1).padStart(2, '0');
 const date = String(d.getDate()).padStart(2, '0');
 
-// 입력 정보 가져오기
-let [$userName, $userEmail, $userPW, $uerPW2, $] =
-  document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
-console.log($input);
+// 수강생 리스트
+const userList = [];
 
-let $bio = document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
-let $gender = document.getElementById('male');
-let $phone;
-let $agreement = document.getElementById('agreement');
+function checkValidation() {
+  return true;
+}
+
+function emailValidation() {
+  let [$userName, $userEmail, $userPW, $userPW2, $userPhoneNumber] =
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
+
+  if (!$userEmail.value.includes('@')) {
+    let temp = `
+      <div class="CSS_입력값오류">
+        <div class="CSS_입력값오류_아이템">
+          비밀번호와 비밀번호 확인이 일치하지 않습니다.
+        </div>
+      </div>
+    `;
+  }
+
+  // email 영역 검사
+  const [userName, domain] = $userEmail.value.split('@');
+  // @가 없거나, (naver, gmail, hanmail.net, kakao.com) 이 아니면
+}
+function passwordValidation() {
+  let [$userName, $userEmail, $userPW, $userPW2, $userPhoneNumber] =
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
+
+  // 비밀번호 영역 검사
+  if ($userPW.value !== $userPW2.value) {
+    let temp = `
+      <div class="CSS_입력값오류">
+        <div class="CSS_입력값오류_아이템">
+          비밀번호와 비밀번호 확인이 일치하지 않습니다.
+        </div>
+      </div>
+    `;
+    document.getElementById('비밀번호값오류안내').innerHTML = temp;
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[2].style =
+      'border: 1px solid #E84F4F;';
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[3].style =
+      'border: 1px solid #E84F4F;';
+  } else {
+    document.getElementById('비밀번호값오류안내').innerHTML = '';
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[2].style = '';
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[3].style = '';
+  }
+}
+
+function phoneValidation() {
+  let [$userName, $userEmail, $userPW, $userPW2, $userPhoneNumber] =
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
+
+  // 전화번호 영역 검사
+  // 전화번호가 '-'를 갖지 않으면, 경고 문구 출력
+  if ($userPhoneNumber.value && !$userPhoneNumber.value.includes('-')) {
+    let temp = `
+      <div class="CSS_입력값오류">
+        <div class="CSS_입력값오류_아이템">
+          전화번호 입력시 ‘-’를 입력해주세요 ex)010-1234-5678
+        </div>
+      </div>
+    `;
+    document.getElementById('전화번호값오류안내').innerHTML = temp;
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[4].style =
+      'border: 1px solid #E84F4F;';
+  } else {
+    document.getElementById('전화번호값오류안내').innerHTML = '';
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋')[4].style = '';
+  }
+}
+
+function appendUserList() {
+  // user 데이터 추가
+  userList.push(1);
+
+  // 업데이트된 태그를 담을 변수
+  let updatedUserList = '';
+
+  // 추가된 userList만큼 업데이트
+  for (let i = 0; i < userList.length; i++) {
+    const user = `<div>수강생${i + 1}</div>`;
+    updatedUserList += user;
+  }
+
+  // 업데이트 반영
+  document.getElementsByClassName(
+    'CSS_사이드바_왼쪽부분_항목_제목'
+  )[1].innerHTML = `${updatedUserList}`;
+}
 
 function onSignUpButton() {
-  // '가입' 버튼을 누르면, 가입 축하 문구와 입력 정보를 alert 창에 출력
+  // '가입' 버튼을 누르면,
+  // 가입 축하 문구와 입력 정보를 alert 창에 출력
+  // 수강생 추가
   printWelcomMessage();
-  printUserInfo();
+  appendUserList();
 }
 
 function printUserInfo() {
-  let userInfo;
+  // 입력 정보 가져오기
+  let [$userName, $userEmail, $userPW, $uerPW2, $userPhoneNumber] =
+    document.getElementsByClassName('CSS_사이드바_회원가입_인풋');
+  $userPW.value = '*'.repeat($userPW.value.length);
+
+  let $bio =
+    document.getElementsByClassName('CSS_사이드바_회원가입_자기소개')[0];
+  let $gender = document.getElementById('male');
+  let $agreement = document.getElementById('agreement');
+  const userInfo =
+    `이름: ${$userName.value ?? ''}\n` +
+    `이메일: ${$userEmail.value ?? ''}\n` +
+    `비밀번호: ${$userPW.value ?? ''}\n` +
+    `성별: ${
+      $gender.value === '남성' ? '남성' : $gender.value === '여성' ? '여성' : ''
+    }\n` +
+    `전화번호: ${$userPhoneNumber.value}\n` +
+    `동의여부: ${$agreement.value === true ? 'Y' : 'N'}\n` +
+    `자기소개: ${$bio.value ?? ''}\n` +
+    `(가입일시: ${year}-${month}-${date})`;
   alert(userInfo);
 }
 
